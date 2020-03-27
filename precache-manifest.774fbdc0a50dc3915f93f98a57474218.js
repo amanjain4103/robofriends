abbb3dfdb2655a7cb5e7e14aff01dae797f83044
amanjain4103 <amanjain4103@gmail.com>
2020-03-27 18:34:12 +0530

    Updates

---
+++ precache-manifest.774fbdc0a50dc3915f93f98a57474218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810db49f6f3308522374a888cf6f3ae7",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c8e266cc4def3965e0e0",
+    "url": "/robofriends/static/css/main.e09c492e.chunk.css"
+  },
+  {
+    "revision": "c4b29f1bd78cdb943d25",
+    "url": "/robofriends/static/js/2.ce957640.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.ce957640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e266cc4def3965e0e0",
+    "url": "/robofriends/static/js/main.7c2206bd.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
